ca4ba88e2677982bda944544497bf10c8d2d2790
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-08 15:25:05 -0400

    WIP

--- src/widgets/markdown/serializers/remarkSquashReferences.ts
+++ src/widgets/markdown/serializers/remarkSquashReferences.ts
@@ -1,7 +1,11 @@
 import without from 'lodash/without';
 import flatten from 'lodash/flatten';
 import u from 'unist-builder';
-import mdastDefinitions from 'mdast-util-definitions';
+import { definitions } from 'mdast-util-definitions';
+
+import type { Transformer } from 'unified';
+import type { Root, Content } from 'mdast';
+import type { Definition } from 'mdast-util-definitions';
 
 /**
  * Raw markdown may contain image references or link references. Because there
@@ -24,15 +28,8 @@ import mdastDefinitions from 'mdast-util-definitions';
  * ```
  *
  */
-export default function remarkSquashReferences() {
-  return getTransform;
-
-  function getTransform(node) {
-    const getDefinition = mdastDefinitions(node);
-    return transform.call(null, getDefinition, node);
-  }
-
-  function transform(getDefinition, node) {
+const remarkSquashReferences: () => Transformer<Root> = () => {
+  function transform(getDefinition: (identifier: string) => Definition | null, node: Root): Root {
     /**
      * Bind the `getDefinition` function to `transform` and recursively map all
      * nodes.
@@ -67,8 +64,17 @@ export default function remarkSquashReferences() {
       return null;
     }
 
-    const filteredChildren = without(children, null);
+    const filteredChildren = without(children, null) as Content[];
 
     return { ...node, children: flatten(filteredChildren) };
   }
-}
+
+  function getTransform(node: Root) {
+    const getDefinition = definitions(node);
+    return transform.call(null, getDefinition, node);
+  }
+
+  return getTransform;
+};
+
+export default remarkSquashReferences;
