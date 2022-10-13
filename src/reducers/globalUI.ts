b3afb6e189fc74eb82eb8c9e87d0aef09f9eb219
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-12 20:13:16 -0400

    WIP

--- src/reducers/globalUI.ts
+++ src/reducers/globalUI.ts
@@ -12,9 +12,8 @@ const defaultState: GlobalUIState = {
  * Reducer for some global UI state that we want to share between components
  */
 const globalUI = (state: GlobalUIState = defaultState, action: AnyAction): GlobalUIState => {
-  console.log(action);
   // Generic, global loading indicator
-  if (!action.type.includes('REQUEST')) {
+  if (action.type.includes('REQUEST')) {
     return {
       isFetching: true,
     };
