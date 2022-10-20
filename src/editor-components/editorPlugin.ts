72497d44bd2d7def2d706881551b97278dff2de1
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-20 12:58:32 -0400

    WIP

--- src/editor-components/editorPlugin.ts
+++ src/editor-components/editorPlugin.ts
@@ -1,20 +1,56 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 
-import type { EditorProps } from '@toast-ui/react-editor';
-import type { PluginContext } from '@toast-ui/editor/types/editor';
-import type { Field } from '../interface';
-
-export interface ShortCodePluginProps {
-  fields: Field;
+import type { PluginContext } from '@toast-ui/editor';
+import type { EditorPlugin } from '@toast-ui/editor/types/editor';
+import type { ToolbarItemOptions } from '@toast-ui/editor/types/ui';
+export interface ImagePluginProps {
+  openMediaLibrary: (forImages: boolean) => void;
 }
 
-const useShortCodePlugin = (_props: ShortCodePluginProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const plugin: Required<EditorProps>['plugins'][number] = useCallback((_context: PluginContext, _options?: any) => {
-    return null;
+const PREFIX = 'toastui-editor-';
+
+const useImagePlugin = ({
+  openMediaLibrary,
+}: ImagePluginProps): [EditorPlugin, ToolbarItemOptions] => {
+  const toolbarButton = useMemo(() => {
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.className = 'image toastui-editor-toolbar-icons';
+    btn.ariaLabel = 'Insert image';
+    btn.setAttribute('style', 'margin: 0;');
+    return btn;
   }, []);
 
-  return plugin;
+  useEffect(() => {
+    const handler = () => {
+      openMediaLibrary(true);
+    };
+
+    toolbarButton.addEventListener('click', handler);
+
+    return () => {
+      toolbarButton.removeEventListener('click', handler);
+    };
+  }, [openMediaLibrary, toolbarButton]);
+
+  const toolbarItem: ToolbarItemOptions = useMemo(
+    () => ({
+      name: 'cmsimage',
+      className: `${PREFIX}toolbar-icons color`,
+      el: toolbarButton,
+    }),
+    [toolbarButton],
+  );
+
+  const plugin: EditorPlugin = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (_context: PluginContext, _options?: any) => {
+      return {};
+    },
+    [],
+  );
+
+  return [plugin, toolbarItem];
 };
 
-export default useShortCodePlugin;
+export default useImagePlugin;
--- src/widgets/markdown/MarkdownControl.tsx
+++ src/widgets/markdown/MarkdownControl.tsx
@@ -1,19 +1,23 @@
 import { styled } from '@mui/material/styles';
 import { Editor } from '@toast-ui/react-editor';
 import mime from 'mime-types';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import uuid from 'uuid';
 
 import FieldLabel from '../../components/UI/FieldLabel';
 import Outline from '../../components/UI/Outline';
+import useImagePlugin from '../../editor-components/editorPlugin';
 import { sanitizeSlug } from '../../lib/urlHelper';
 import { selectMediaFilePath } from '../../lib/util/media.util';
 import { createAssetProxy } from '../../valueObjects/AssetProxy';
 
 import type { RefObject } from 'react';
-import type { MarkdownField, WidgetControlProps } from '../../interface';
+import type { MarkdownField, MediaLibrary, WidgetControlProps } from '../../interface';
 
 import '@toast-ui/editor/dist/toastui-editor.css';
+import isEmpty from 'lodash/isEmpty';
+import { doesUrlFileExist } from '../../lib/util/fetch.util';
+import { isNotNullish } from '../../lib/util/null.util';
 
 const StyledEditorWrapper = styled('div')`
   position: relative;
@@ -44,9 +48,12 @@ const MarkdownControl = ({
   field,
   addAsset,
   addDraftEntryMediaFile,
+  openMediaLibrary,
   config,
   collection,
   entry,
+  mediaPaths,
+  getAsset
 }: WidgetControlProps<string, MarkdownField>) => {
   const [internalValue, setInternalValue] = useState(value ?? '');
   const editorRef = useMemo(() => React.createRef(), []) as RefObject<Editor>;
@@ -70,43 +77,80 @@ const MarkdownControl = ({
     editorRef.current?.getInstance().focus();
   }, [editorRef]);
 
-  const imageUpload = useCallback(
-    (blob: Blob | File, callback: (url: string, text?: string) => void) => {
-      let file: File;
-      if (blob instanceof Blob) {
-        blob.type;
-        file = new File([blob], `${uuid()}.${mime.extension(blob.type)}`);
-      } else {
-        file = blob;
-      }
-      const fileName = sanitizeSlug(file.name.toLowerCase(), config.slug);
-      const path = selectMediaFilePath(config, collection, entry, fileName, field);
-      const blobUrl = URL.createObjectURL(file);
-      addAsset(
-        createAssetProxy({
-          url: blobUrl,
-          file,
-          path,
-          field,
-        }),
-      );
-      addDraftEntryMediaFile({
-        name: file.name,
-        id: file.name,
-        size: file.size,
-        displayURL: blobUrl,
-        path,
-        draft: true,
-        url: blobUrl,
-        file,
-        field,
-      });
-      console.log(blob);
-      callback(path);
-      handleOnChange();
-    },
-    [addAsset, addDraftEntryMediaFile, collection, config, entry, field, handleOnChange],
+  const controlID: string = useMemo(() => uuid(), []);
+  const mediaLibraryFieldOptions: MediaLibrary = useMemo(
+    () => field.media_library ?? {},
+    [field.media_library],
   );
+  const handleOpenMedialLibrary = useCallback(
+    (forImage: boolean) => {
+      openMediaLibrary({
+        controlID,
+        forImage,
+        privateUpload: false,
+        allowMultiple: false,
+        field,
+        config: 'config' in mediaLibraryFieldOptions ? mediaLibraryFieldOptions.config : undefined,
+      });
+    },
+    [controlID, field, mediaLibraryFieldOptions, openMediaLibrary],
+  );
+
+  const [imagePlugin, imageToolbarButton] = useImagePlugin({ openMediaLibrary: handleOpenMedialLibrary });
+
+  const getMedia = useCallback(
+    async (path: string) => {
+      const { type, exists } = await doesUrlFileExist(path);
+      if (!exists) {
+        const asset = getAsset(path, field);
+        if (isNotNullish(asset)) {
+          return {
+            type: IMAGE_EXTENSION_REGEX.test(path) ? 'image' : 'file',
+            exists: false,
+            url: asset.toString()
+          };
+        }
+      }
+
+      return { url: path, type, exists };
+    },
+    [field, getAsset]
+  );
+
+  const mediaPath = mediaPaths[controlID];
+  useEffect(() => {
+    if (isEmpty(mediaPath) || Array.isArray(mediaPath)) {
+      return;
+    }
+
+    const addMedia = async () => {
+      const { type, exists, url } = await getMedia(mediaPath);
+      let content: string | undefined;
+      if (type.startsWith('image')) {
+        if (exists) {
+          content = `<img src="${mediaPath}" />`;
+        } else {
+          content = `<img data-asset="${mediaPath}" src="${url}" />`;
+        }
+      } else {
+        const name = mediaPath.split('/').pop();
+        if (exists) {
+          content = `<a target="_blank" href="${mediaPath}">${name}</a>`;
+        } else {
+          content = `<a data-asset="${mediaPath}" target="_blank" href="${url}">${name}</a>`;
+        }
+      }
+
+      if (isNotEmpty(content)) {
+        editorRef.current.focus();
+        editorRef.current.selection.setContent(content);
+        onChange(editorRef.current.getContent());
+      }
+    };
+
+    addMedia();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [field, mediaPath]);
 
   return (
     <StyledEditorWrapper key="markdown-control-wrapper">
@@ -126,19 +170,17 @@ const MarkdownControl = ({
         initialEditType="markdown"
         useCommandShortcut={true}
         onChange={handleOnChange}
+        plugins={[imagePlugin]}
         toolbarItems={[
           ['heading', 'bold', 'italic', 'strike'],
           ['hr', 'quote'],
           ['ul', 'ol', 'task', 'indent', 'outdent'],
-          ['table', 'image', 'link'],
+          ['table', imageToolbarButton, 'link'],
           ['code', 'codeblock'],
         ]}
         ref={editorRef}
         onFocus={handleOnFocus}
         onBlur={handleOnBlur}
-        hooks={{
-          addImageBlobHook: imageUpload,
-        }}
         autofocus={false}
       />
       <Outline key="markdown-control-outline" hasLabel hasError={hasErrors} />
