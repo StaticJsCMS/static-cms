c934384584e420df8c7ec38661a3296222dd4cc6
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-21 10:00:55 -0400

    WIP

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,8 +1,10 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import Page from '../components/layout/Page';
 import styles from '../styles/Home.module.css';
+
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
