d8c776123d6aad0a50afb500d3b583ba0ae793f8
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-21 22:20:16 -0400

    WIP

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,18 +6,22 @@ import Link from 'next/link';
 import Page from '../components/layout/Page';
 
 import type { NextPage } from 'next';
+
 const StyledHomagePageContent = styled('div')`
   padding-top: 64px;
+`;
+
+const StyledFeaturesSection = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(minmax(0, 1fr), 2);
 `;
 
 const Home: NextPage = () => {
   return (
     <Page url="/">
       <StyledHomagePageContent>
-        <Typography variant="h1">
-          Open source content management for your Git workflow
-        </Typography>
-        <Typography variant="h5" color="text.primary" sx={{ mt: 2 }}>
+        <Typography variant="h1">Open source content management for your Git workflow</Typography>
+        <Typography variant="h5" component="h2" color="text.primary" sx={{ mt: 2 }}>
           Use Static CMS with any static site generator for a faster and more flexible web project
         </Typography>
         <Link href="/docs/start-with-a-template">
@@ -25,32 +29,11 @@ const Home: NextPage = () => {
             Get Started
           </Button>
         </Link>
-
-        <p>
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
-
-        <div>
-          <a href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <StyledFeaturesSection>
+          <Typography variant="h3" component="h2" color="text.primary" sx={{ mt: 2 }}>
+            Use Static CMS with any static site generator for a faster and more flexible web project
+          </Typography>
+        </StyledFeaturesSection>
       </StyledHomagePageContent>
     </Page>
   );
