daf44f9dc2b4f73c939200baea7066ed99078e3c
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-25 16:22:01 -0400

    Add next blog template

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Prism from 'prismjs';
 import { useEffect, useMemo, useState } from 'react';
-import { useRouter } from 'next/router';
 
 import ColorModeContext from '../components/context/ColorModeContext';
+import homepageData from '../lib/homepage';
 import useCreateTheme from '../styles/theme';
 
 import type { PaletteMode } from '@mui/material';
@@ -47,12 +49,28 @@ function MyApp({ Component, pageProps }: AppProps) {
   const theme = useCreateTheme(mode);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#2e3034" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="description" content={homepageData.title} />
+      </Head>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
   );
 }
 
--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -1,24 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-import homepageData from '../lib/homepage';
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-        />
-        <meta name="theme-color" content="#2e3034" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="description" content={homepageData.title} />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
