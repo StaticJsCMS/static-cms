ad0b1a4179ffeffd14f338d25e414eb21aa11e89
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-26 20:37:29 -0400

    WIP

--- core/src/widgets/markdown/config/widgetRules.ts
+++ core/src/widgets/markdown/config/widgetRules.ts
@@ -6,16 +6,19 @@ const defaultWidgetRules: WidgetRulesFactory = ({ getAsset, field }) => [
   {
     rule: imageFilePattern,
     toDOM(text) {
+      console.log('[MARKDOWN] hello');
       const rule = imageFilePattern;
       const matched = text.match(rule);
 
       if (matched) {
+        console.log('[MARKDOWN] matched', matched);
         if (matched?.length === 4) {
           // Image
           const img = document.createElement('img');
-          img.setAttribute('src', getAsset(matched[3] ?? '', field).url);
+          img.setAttribute('src', getAsset(matched[3] ?? '', field).url ?? matched[3]);
           img.setAttribute('style', 'width: 100%;');
-          img.innerHTML = matched[2] ?? '';
+          img.setAttribute('title', matched[2] ?? '');
+          console.log('[MARKDOWN] img', img);
           return img;
         } else {
           // File
@@ -23,10 +26,12 @@ const defaultWidgetRules: WidgetRulesFactory = ({ getAsset, field }) => [
           a.setAttribute('target', '_blank');
           a.setAttribute('href', matched[2] ?? '');
           a.innerHTML = matched[1] ?? '';
+          console.log('[MARKDOWN] a', a);
           return a;
         }
       }
 
+      console.log('[MARKDOWN] missed', text);
       return document.createElement('div');
     },
   },
