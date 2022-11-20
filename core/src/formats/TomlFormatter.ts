c597e08c098c746ebadb0c96834da5f1b606b714
Daniel Lautzenheiser <lautzd@gmail.com>
2022-11-20 09:19:33 -0500

    Remove dead code

--- core/src/formats/TomlFormatter.ts
+++ core/src/formats/TomlFormatter.ts
@@ -1,25 +1,6 @@
 import toml from '@ltd/j-toml';
-import moment from 'moment';
 
-import AssetProxy from '../valueObjects/AssetProxy';
 import { FileFormatter } from './FileFormatter';
-
-function outputReplacer(_key: string, value: unknown) {
-  if (moment.isMoment(value)) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return value.format(value._f);
-  }
-  if (value instanceof AssetProxy) {
-    return `${value.path}`;
-  }
-  if (typeof value === 'number' && Number.isInteger(value)) {
-    // Return the string representation of integers so tomlify won't render with tenths (".0")
-    return value.toString();
-  }
-  // Return `false` to use default (`undefined` would delete key).
-  return false;
-}
 
 class TomlFormatter extends FileFormatter {
   fromFile(content: string) {
