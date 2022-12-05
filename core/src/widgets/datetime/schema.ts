01424d1c57a5db05b9a508b0535ef9bbe19a1962
Daniel Lautzenheiser <lautzd@gmail.com>
2022-12-05 16:28:07 -0500

    WIP

--- core/src/widgets/datetime/schema.ts
+++ core/src/widgets/datetime/schema.ts
@@ -4,5 +4,6 @@ export default {
     date_format: { oneOf: [{ type: 'string' }, { type: 'boolean' }] },
     time_format: { oneOf: [{ type: 'string' }, { type: 'boolean' }] },
     picker_utc: { type: 'boolean' },
+    default: { type: 'string' },
   },
 };
