056496bf258ae954e81b03e9931e2a197133376a
Daniel Lautzenheiser <lautzd@gmail.com>
2022-12-04 22:05:38 -0500

    WIP

--- core/test/mockFetch.ts
+++ core/test/mockFetch.ts
@@ -1,11 +1,37 @@
-export const createMockRequest = <T>(status: number, data: T, contentType = 'application/json'): Response => {
-  const headers = new Headers();
-  headers.set('Content-Type', contentType);
+export const createMockRequest = <T>(
+  status: number,
+  data: {
+    json?: T;
+    text?: string;
+  },
+  options: {
+    contentType?: string;
+    headers?: Record<string, string>;
+  } = {},
+): Response => {
+  const { contentType = 'application/json', headers = {} } = options;
+
+  const finalHeaders = (function () {
+    const data: Record<string, string> = headers;
+
+    return {
+      get: (key: string) => {
+        return data[key];
+      },
+      set: (key: string, value: string) => {
+        data[key] = value;
+      },
+    };
+  })();
+
+  finalHeaders.set('Content-Type', contentType);
 
   return {
     status,
-    headers,
-    json: () => Promise.resolve(data),
+    ok: status < 400,
+    headers: finalHeaders,
+    json: () => Promise.resolve(data.json),
+    text: () => Promise.resolve(data.text),
   } as Response;
 };
 
@@ -13,7 +39,17 @@ export interface MockFetch {
   baseUrl: string;
   mocks: Record<string, Response>;
   when: (url: string) => {
-    reply: <T>(status: number, data: T) => void;
+    reply: <T>(
+      status: number,
+      data: {
+        json?: T;
+        text?: string;
+      },
+      options?: {
+        contentType?: string;
+        headers?: Record<string, string>;
+      },
+    ) => void;
   };
   reset: () => void;
 }
@@ -24,9 +60,18 @@ const mockFetch = (baseUrl: string): MockFetch => {
     mocks: {},
     when(this: MockFetch, url: string) {
       return {
-        reply: <T>(status: number, data: T, contentType?: string) => {
-          console.log(`${baseUrl}${url}`);
-          this.mocks[`${baseUrl}${url}`] = createMockRequest(status, data, contentType);
+        reply: <T>(
+          status: number,
+          data: {
+            json?: T;
+            text?: string;
+          },
+          options?: {
+            contentType?: string;
+            headers?: Record<string, string>;
+          },
+        ) => {
+          this.mocks[`${baseUrl}${url}`] = createMockRequest(status, data, options);
         },
       };
     },
@@ -36,7 +81,7 @@ const mockFetch = (baseUrl: string): MockFetch => {
   };
 
   global.fetch = jest.fn().mockImplementation((url: string) => {
-    return Promise.resolve(mockedFetch.mocks[url]);
+    return Promise.resolve(mockedFetch.mocks[url.split('?')[0]]);
   });
 
   return mockedFetch;
