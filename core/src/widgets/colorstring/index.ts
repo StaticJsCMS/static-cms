7b8d9bbe6bf3affdf80d3b90924bf9f2ff45f977
Daniel Lautzenheiser <lautzd@gmail.com>
2022-12-06 08:09:48 -0500

    WIP

--- core/src/widgets/colorstring/index.ts
+++ core/src/widgets/colorstring/index.ts
@@ -1,5 +1,6 @@
 import controlComponent from './ColorControl';
 import previewComponent from './ColorPreview';
+import schema from './schema';
 
 import type { ColorField, WidgetParam } from '@staticcms/core/interface';
 
@@ -8,9 +9,16 @@ const ColorWidget = (): WidgetParam<string, ColorField> => {
     name: 'color',
     controlComponent,
     previewComponent,
+    options: {
+      schema,
+    },
   };
 };
 
-export { controlComponent as ColorControl, previewComponent as ColorPreview };
+export {
+  controlComponent as ColorControl,
+  previewComponent as ColorPreview,
+  schema as ColorSchema,
+};
 
 export default ColorWidget;
