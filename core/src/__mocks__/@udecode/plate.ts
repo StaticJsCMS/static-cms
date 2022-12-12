c830ce3e42ddc2690ad36857b774ef250b303972
Daniel Lautzenheiser <lautzd@gmail.com>
2022-12-12 10:52:42 -0500

    Split tests out into integration and unit

--- core/src/__mocks__/@udecode/plate.ts
+++ core/src/__mocks__/@udecode/plate.ts
@@ -1,24 +1,22 @@
-import {
-  ELEMENT_BLOCKQUOTE,
-  ELEMENT_CODE_BLOCK,
-  ELEMENT_H1,
-  ELEMENT_H2,
-  ELEMENT_H3,
-  ELEMENT_H4,
-  ELEMENT_H5,
-  ELEMENT_H6,
-  ELEMENT_IMAGE,
-  ELEMENT_LI,
-  ELEMENT_LIC,
-  ELEMENT_LINK,
-  ELEMENT_OL,
-  ELEMENT_PARAGRAPH,
-  ELEMENT_TABLE,
-  ELEMENT_TD,
-  ELEMENT_TH,
-  ELEMENT_TR,
-  ELEMENT_UL,
-} from '@udecode/plate';
+const ELEMENT_BLOCKQUOTE = 'blockquote';
+const ELEMENT_CODE_BLOCK = 'code_block';
+const ELEMENT_H1 = 'h1';
+const ELEMENT_H2 = 'h2';
+const ELEMENT_H3 = 'h3';
+const ELEMENT_H4 = 'h4';
+const ELEMENT_H5 = 'h5';
+const ELEMENT_H6 = 'h6';
+const ELEMENT_IMAGE = 'img';
+const ELEMENT_LI = 'li';
+const ELEMENT_LIC = 'lic';
+const ELEMENT_LINK = 'a';
+const ELEMENT_OL = 'ol';
+const ELEMENT_PARAGRAPH = 'p';
+const ELEMENT_TABLE = 'table';
+const ELEMENT_TD = 'td';
+const ELEMENT_TH = 'th';
+const ELEMENT_TR = 'tr';
+const ELEMENT_UL = 'ul';
 
 export {
   ELEMENT_BLOCKQUOTE,
