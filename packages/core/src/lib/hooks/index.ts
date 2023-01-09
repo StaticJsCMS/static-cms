a9e0770fd07664969a01b9dd24be89c8ee281cf2
Daniel Lautzenheiser <lautzd@gmail.com>
2023-01-09 14:32:41 -0500

    fix: export useMediaInsert into index

--- packages/core/src/lib/hooks/index.ts
+++ packages/core/src/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useIsMediaAsset } from './useIsMediaAsset';
 export { default as useMediaAsset } from './useMediaAsset';
 export { default as useMediaInsert } from './useMediaInsert';
+export { default as useUUID } from './useMediaInsert';
