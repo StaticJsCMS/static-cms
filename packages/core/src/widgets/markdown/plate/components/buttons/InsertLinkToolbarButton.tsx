4f81c946c8757afcd01d84a3c443ae6b8e8825ca
Daniel Lautzenheiser <lautzd@gmail.com>
2023-05-12 07:31:08 -0400

    WIP

--- packages/core/src/widgets/markdown/plate/components/buttons/InsertLinkToolbarButton.tsx
+++ packages/core/src/widgets/markdown/plate/components/buttons/InsertLinkToolbarButton.tsx
@@ -1,23 +1,23 @@
 import { Link as LinkIcon } from '@styled-icons/material/Link';
 import {
   ELEMENT_LINK,
-  focusEditor,
+  getAboveNode,
+  getNode,
   getSelectionText,
   insertLink,
-  replaceNodeChildren,
   setNodes,
   someNode,
-  upsertLink,
 } from '@udecode/plate';
 import React, { useCallback, useMemo, useState } from 'react';
 
 import useMediaInsert from '@staticcms/core/lib/hooks/useMediaInsert';
 import useUUID from '@staticcms/core/lib/hooks/useUUID';
-import { isEmpty, isNotEmpty } from '@staticcms/core/lib/util/string.util';
+import { isNotEmpty } from '@staticcms/core/lib/util/string.util';
 import { useMdPlateEditorState } from '@staticcms/markdown/plate/plateTypes';
 import ToolbarButton from './common/ToolbarButton';
 
 import type { Collection, MarkdownField, MediaPath } from '@staticcms/core/interface';
+import type { MdLinkElement } from '@staticcms/markdown/plate/plateTypes';
 import type { FC } from 'react';
 import type { BaseSelection } from 'slate';
 
@@ -40,14 +40,60 @@ const InsertLinkToolbarButton: FC<InsertLinkToolbarButtonProps> = ({
   const editor = useMdPlateEditorState();
   const handleInsert = useCallback(
     ({ path: newUrl, alt: newText }: MediaPath<string>) => {
-      if (isNotEmpty(newUrl) && selection) {
-        focusEditor(editor, selection);
-
-        upsertLink(editor, {
+      if (isNotEmpty(newUrl)) {
+        const text = isNotEmpty(newText) ? newText : newUrl;
+        const link: MdLinkElement = {
+          type: ELEMENT_LINK,
           url: newUrl,
-          text: newText ?? newUrl,
-          insertNodesOptions: { at: selection },
+          children: [{ text }],
+        };
+
+        const linkAbove = getAboveNode<MdLinkElement>(editor, {
+          at: selection?.focus,
+          match: { type: ELEMENT_LINK },
         });
+
+        if (linkAbove) {
+          if (newUrl !== linkAbove[0]?.url || text !== linkAbove[0]?.children[0].text) {
+            setNodes<MdLinkElement>(editor, link, {
+              at: linkAbove[1],
+            });
+          }
+
+          return;
+        }
+
+        if (selection) {
+          const linkAt = getNode<MdLinkElement>(editor, selection.anchor.path);
+
+          if (linkAt && linkAt?.type === ELEMENT_LINK) {
+            if (newUrl !== linkAt.url || text !== linkAt.children[0].text) {
+              setNodes<MdLinkElement>(editor, link, {
+                at: selection,
+              });
+            }
+
+            return;
+          }
+        }
+
+        if (linkAbove) {
+          if (newUrl !== linkAbove[0]?.url || text !== linkAbove[0]?.children[0].text) {
+            setNodes<MdLinkElement>(editor, link, {
+              at: linkAbove[1],
+            });
+          }
+
+          return;
+        }
+
+        insertLink(
+          editor,
+          { url: newUrl, text },
+          {
+            at: selection?.focus,
+          },
+        );
       }
     },
     [editor, selection],
