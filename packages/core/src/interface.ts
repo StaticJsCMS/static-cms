1c8ff049c5cfd16d77a0c699fc060ebf0ae28de1
Daniel Lautzenheiser <lautzd@gmail.com>
2023-09-07 08:54:59 -0400

    feat: cache search results (#869)

--- packages/core/src/interface.ts
+++ packages/core/src/interface.ts
@@ -1037,6 +1037,12 @@ export interface SearchResponse {
   pagination: number;
 }
 
+export type SearchQueryRequest = {
+  id: string;
+  expires: Date;
+  queryResponse: Promise<SearchQueryResponse>;
+};
+
 export interface SearchQueryResponse {
   hits: Entry[];
   query: string;
