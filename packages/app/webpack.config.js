20b766066990de9de2d144df8b4b1fd779bb8b80
Daniel Lautzenheiser <lautzd@gmail.com>
2023-10-31 09:00:53 -0400

    Update webpack.config.js

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const pkg = require('./package.json');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const devServerPort = parseInt(process.env.STATIC_CMS_DEV_SERVER_PORT || `${8080}`);
